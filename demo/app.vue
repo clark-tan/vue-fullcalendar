af06be3640630ba6008076a1c254b89ec6126cca
clark <tanyachao0729@hotmail.com>
2019-03-13 13:54:11 +0800

    修复fc-event-card自定义bug

--- demo/app.vue
+++ demo/app.vue
@@ -1,80 +1,92 @@
 <template>
   <div>
-  	<p>
-  		Here is the demo of vue-fullcalendar, no jquery fullcalendar.js required!
-  	</p>
-    <full-calendar class="test-fc" :events="fcEvents" :days-extend="daysExtend"
-      first-day='1' locale="zh_CN" :weekEnd="weekEnd"
+    <p>Here is the demo of vue-fullcalendar, no jquery fullcalendar.js required!</p>
+    <full-calendar
+      class="test-fc"
+      :events="fcEvents"
+      :days-extend="daysExtend"
+      first-day="1"
+      locale="zh_CN"
+      :weekEnd="weekEnd"
       @changeMonth="changeMonth"
       @eventClick="eventClick"
       @dayClick="dayClick"
-      @moreClick="moreClick">
-        <template slot="fc-event-card" scope="p">
-            <p><i class="fa">sadfsd</i> {{ p.event.title }} test</p>
-        </template>
+      @moreClick="moreClick"
+    >
+      <template slot="fc-event-card" slot-scope="p">
+        <p>
+          <i class="fa">sadfsd</i>
+          {{ p.event.title + '----'}}---
+        </p>
+      </template>
     </full-calendar>
   </div>
 </template>
 <script>
-let weekEnd = [5,6];
+let weekEnd = [5, 6];
 let demoEvents = [
-    {
-      title : 'Lunfel',
-      start : '2017-03-27',
-      end : '2017-03-28'
-    },
-    {
-      title : 'Lunfel1',
-      start : '2017-03-27',
-      end : '2017-03-28'
-    },
-    {
-      title : 'Lunfel2',
-      start : '2017-03-27',
-      end : '2017-03-28'
-    },
-    {
-      title : 'Lunfel3',
-      start : '2017-03-27',
-      end : '2017-03-28'
-    }
-  ];
+  {
+    title: "Lunfel",
+    start: "2019-03-27",
+    end: "2019-03-28"
+  },
+  {
+    title: "Lunfel1",
+    start: "2019-03-27",
+    end: "2019-03-28"
+  },
+  {
+    title: "Lunfel2",
+    start: "2019-03-27",
+    end: "2019-03-28"
+  },
+  {
+    title: "Lunfel3",
+    start: "2019-03-27",
+    end: "2019-03-28"
+  }
+];
 
 export default {
-	data () {
-		return {
-            name:'Sunny!',
-            weekEnd,
-            fcEvents : demoEvents,
-            daysExtend: [
-              {
-                date: '2017-03-10',
-                cssClass: ['red']
-              }
-            ]
+  data() {
+    return {
+      name: "Sunny!",
+      weekEnd,
+      fcEvents: demoEvents,
+      daysExtend: [
+        {
+          date: "2019-03-10",
+          cssClass: ["red"]
         }
-	},
-  methods : {
-    'changeMonth' (start, end, current) {
-      console.log('changeMonth', start.format(), end.format(), current.format())
+      ]
+    };
+  },
+  methods: {
+    changeMonth(start, end, current) {
+      console.log(
+        "changeMonth",
+        start.format(),
+        end.format(),
+        current.format()
+      );
     },
-    'eventClick' (event, jsEvent, pos) {
-       console.log('eventClick', event, jsEvent, pos)
+    eventClick(event, jsEvent, pos) {
+      console.log("eventClick", event, jsEvent, pos);
     },
-    'dayClick' (day, jsEvent) {
-      console.log('dayClick', day, jsEvent)
+    dayClick(day, jsEvent) {
+      console.log("dayClick", day, jsEvent);
     },
-    'moreClick' (day, events, jsEvent) {
-      console.log('moreCLick', day, events, jsEvent)
+    moreClick(day, events, jsEvent) {
+      console.log("moreCLick", day, events, jsEvent);
     }
   },
-  components : {
-    'full-calendar' : require('src/fullCalendar')
+  components: {
+    "full-calendar": require("src/fullCalendar")
   }
-}
+};
 </script>
 <style lang='scss'>
-  .app{
-    color:green;
-  }
+.app {
+  color: green;
+}
 </style>
